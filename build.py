ad9bd075fb2a00780390fed1214973a3a3fc3313
Burak Emre Kabakcı <emrekabakci@gmail.com>
2019-08-16 18:26:18 +0300

    Update build.py

--- build.py
+++ build.py
@@ -1,14 +1,26 @@
 import _jsonnet
+import json
+import os
+import requests
 
+GITHUB_API_URL = "https://api.github.com/gists/3261da605c91ed76844e431391b6b91a"
 
 if __name__ == "__main__":
     result = []
-    for root, dirs, files in os.walk(path):
-        if name in files:
-            result.append(os.path.join(root, name))
-    return result
+    for root, dirs, files in os.walk("."):
+        for name in files:
+            if name == '_config.jsonnet':
+                config = json.loads(_jsonnet.evaluate_snippet("snippet", open(os.path.join(root, name), "r").read()))
+                result.append(
+                    {"repository": 'https://github.com/rakam-io/recipes',
+                     "path": root[1:],
+                     "config": config
+                     })
 
-
-json_str = _jsonnet.evaluate_snippet(
-    "snippet", jsonnet_str,
-    ext_vars={'OTHER_NAME': 'Bob'})
+    res = requests.post(GITHUB_API_URL, headers={'Authorization': 'token %s' % os.environ['GITHUB_API_TOKEN']},
+                        params={'scope': 'gist'},
+                        data=json.dumps({"files": {"rakam-recipes.index.json": {"content": json.dumps(result)}}}))
+    if res.status_code != 200:
+        raise Exception(result.content)
+    else:
+        print("Successfully updated!")
