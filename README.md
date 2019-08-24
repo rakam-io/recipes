7dbc9934c0f82730161f4a28275487ea9d404b07
Burak Emre Kabakcı <emrekabakci@gmail.com>
2019-08-24 04:41:47 +0300

    add pipeline

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Recipes
 
+[![wercker status](https://app.wercker.com/status/30b51613b0945eb6ca35bd51a8b2f84f/s/master "wercker status")](https://app.wercker.com/project/byKey/30b51613b0945eb6ca35bd51a8b2f84f)
+
 A collection of Rakam recipes
--- build.py
+++ build.py
@@ -2,6 +2,7 @@ import _jsonnet
 import json
 import os
 import requests
+import sys
 
 GITHUB_API_URL = "https://api.github.com/gists/3261da605c91ed76844e431391b6b91a"
 
@@ -21,10 +22,11 @@ if __name__ == "__main__":
                      "config": config
                      })
 
-    res = requests.post(GITHUB_API_URL, headers={'Authorization': 'token %s' % os.environ['GITHUB_API_TOKEN']},
-                        params={'scope': 'gist'},
-                        data=json.dumps({"files": {"rakam-recipes.index.json": {"content": json.dumps(result)}}}))
-    if res.status_code != 200:
-        raise Exception(result.content)
-    else:
-        print("Successfully updated!")
+    if sys.argv[1] == 'deploy':
+            res = requests.post(GITHUB_API_URL, headers={'Authorization': 'token %s' % os.environ['GITHUB_API_TOKEN']},
+                                params={'scope': 'gist'},
+                                data=json.dumps({"files": {"rakam-recipes.index.json": {"content": json.dumps(result)}}}))
+            if res.status_code != 200:
+                raise Exception(result.content)
+            else:
+                print("Successfully updated!")
