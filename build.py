cf1b52dd0cd028b1f42bcc756268ff46e75a22c5
Burak Emre Kabakcı <emrekabakci@gmail.com>
2019-08-24 05:11:55 +0300

    Update build.py

--- build.py
+++ build.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
             'private_key': os.environ['PRIVATE_KEY'],
         }
         credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict)
-        client = storage.Client(credentials=credentials, project='rakamui-215316')
+        client = storage.Client(credentials=credentials, project= os.environ['PROJECT_ID'])
         bucket = client.get_bucket('rakam-public')
         blob = bucket.blob('rakam-recipes.index.json')
         blob.upload_from_string(json.dumps(result), "application/json")
